973c72bbf8d8a074c5778217fa5fe00a4b426ccf
Carl Wivagg <cwivagg@Carls-MacBook-Pro-2.local>
2016-10-13 07:27:20 -0400

    Add sample files to track.

---
+++ circleOnSphere.m
@@ -0,0 +1,27 @@
+function [ normVector1, normVector2 ] = circleOnSphere( startingPoint, cellLength, cellWidth )
+% This function is vectorized.
+% See circleOnCylinder for calculation explanations.
+
+zero = zeros(size(startingPoint,1),1);
+one = ones(size(startingPoint,1),1);
+
+rodLimit = (cellLength-cellWidth)/2*ones(size(startingPoint,1),1);
+positiveCapPoints = startingPoint(startingPoint(:,1)>rodLimit,:);
+negativeCapPoints = startingPoint(startingPoint(:,1)<=rodLimit,:);
+
+rodLimit = (cellLength-cellWidth)/2*ones(size(positiveCapPoints,1),1);
+equationOfPositivePlane = [ 2*positiveCapPoints(:,1)-2*rodLimit, 2*positiveCapPoints(:,2), 2*positiveCapPoints(:,3), 2*positiveCapPoints(:,2).^2+2*positiveCapPoints(:,3).^2+(2*positiveCapPoints(:,1)-2*rodLimit).*positiveCapPoints(:,1) ];
+rodLimit = (cellLength-cellWidth)/2*ones(size(negativeCapPoints,1),1);
+equationOfNegativePlane = [ 2*negativeCapPoints(:,1)+2*rodLimit, 2*negativeCapPoints(:,2), 2*negativeCapPoints(:,3), 2*negativeCapPoints(:,2).^2+2*negativeCapPoints(:,3).^2+(2*negativeCapPoints(:,1)+2*rodLimit).*negativeCapPoints(:,1) ];
+equationOfPlane = zeros(size(startingPoint,1),4);
+rodLimit = (cellLength-cellWidth)/2*ones(size(startingPoint,1),1);
+equationOfPlane(startingPoint(:,1)>rodLimit,:) = equationOfPositivePlane;
+equationOfPlane(startingPoint(:,1)<=rodLimit,:) = equationOfNegativePlane;
+
+startingVector1 = [ one, zero, (equationOfPlane(:,4)-equationOfPlane(:,1))./equationOfPlane(:,3) ];
+startingVector2 = [ one, (equationOfPlane(:,4)-equationOfPlane(:,1))./equationOfPlane(:,2), zero ];
+
+normVector1 = startingVector1 ./ (sqrt(sum(startingVector1.^2,2))*ones(1,3));
+projVector2 = normVector1 .* (dot(normVector1,startingVector2,2)*ones(1,3));
+perpVector2 = startingVector2 - projVector2;
+normVector2 = perpVector2 ./ (sqrt(sum(perpVector2.^2,2))*ones(1,3));
---
+++ projectOnSphere.m
@@ -0,0 +1,21 @@
+function [ newPoint ] = projectOnSphere( oldPoint, cellLength, cellWidth )
+
+rodLimit = ones(size(oldPoint,1),1)*(cellLength-cellWidth)/2;
+r = cellWidth/2*ones(size(oldPoint,1),3);
+
+northPoints = oldPoint(:,1)>=rodLimit;
+southPoints = oldPoint(:,1)<rodLimit;
+
+rodLimit = ones(sum(northPoints,1),1)*(cellLength-cellWidth)/2;
+oldPoint(northPoints,1) = oldPoint(northPoints,1) - rodLimit;
+rodLimit = ones(sum(southPoints,1),1)*(cellLength-cellWidth)/2;
+oldPoint(southPoints,1) = oldPoint(southPoints,1) + rodLimit;
+
+newPoint = oldPoint.*r ./ (sqrt(sum(oldPoint.^2,2))*ones(1,3));
+
+rodLimit = ones(sum(northPoints,1),1)*(cellLength-cellWidth)/2;
+newPoint(northPoints,1) = newPoint(northPoints,1) + rodLimit;
+rodLimit = ones(sum(southPoints,1),1)*(cellLength-cellWidth)/2;
+newPoint(southPoints,1) = newPoint(southPoints,1) - rodLimit;
+
+end
