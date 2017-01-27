64bf4efce105affe2b01cebc8731f6ae44a2439c
Carl Wivagg <cwivagg@Carls-MacBook-Pro-3.local>
2017-01-27 08:09:20 -0500

    Deleting files moved to helper function folder.

--- inCylinder.m
+++
@@ -1,5 +0,0 @@
-function boolAnswer = inCylinder( point, cellLength, cellWidth )
-% This function is vectorized.
-rodLimit = ones(size(point,1),1)*(cellLength-cellWidth)/2;
-
-boolAnswer = ((point(:,1) <= rodLimit) & (point(:,1) >= -rodLimit));
--- projectOnCylinder.m
+++
@@ -1,9 +0,0 @@
-function [ newPoint ] = projectOnCylinder( oldPoint, cellWidth )
-
-r = cellWidth/2*ones(size(oldPoint,1),2);
-
-newPoint = oldPoint;
-newPoint(:,2:3) = oldPoint(:,2:3).*r ./ (sqrt(sum(oldPoint(:,2:3).^2,2))*ones(1,2));
-
-end
-
